33318ccb75621f2d36b3bcb0e11f63d2afa008b0
yutayamazaki <tppymd@gmail.com>
2020-02-29 00:13:15 +0900

    Create new file formatters.py

---
+++ drf_logger/formatters.py
@@ -0,0 +1,19 @@
+from logging import Formatter
+
+KEYS_TO_IGNORE = (
+    'args', 'created', 'exc_info', 'exc_text', 'family', 'fd', 'filename',
+    'funcName', 'laddr', 'levelname', 'levelno', 'lineno', 'msecs', 'msg',
+    'module', 'name', 'pathname', 'process', 'processName', 'proto', 'raddr',
+    'relativeCreated', 'request', 'stack_info', 'thread', 'threadName', 'type'
+)
+
+
+class SimpleExtraFormatter(Formatter):
+
+    def format(self, record) -> str:
+        extra_txt = ''
+        for k, v in record.__dict__.items():
+            if k not in KEYS_TO_IGNORE:
+                extra_txt += ', {}={}'.format(k, v)
+        message = super().format(record)
+        return message + extra_txt
--- drf_logger/utils.py
+++ drf_logger/utils.py
@@ -1,22 +1,6 @@
 import logging
 
-BUILDIN_KEYS = (
-    'args', 'created', 'exc_info', 'exc_text', 'family', 'fd', 'filename',
-    'funcName', 'laddr', 'levelname', 'levelno', 'lineno', 'msecs', 'msg',
-    'module', 'name', 'pathname', 'process', 'processName', 'proto', 'raddr',
-    'relativeCreated', 'request', 'stack_info', 'thread', 'threadName', 'type'
-)
-
-
-class SimpleExtraFormatter(logging.Formatter):
-
-    def format(self, record) -> str:
-        extra_txt = ''
-        for k, v in record.__dict__.items():
-            if k not in BUILDIN_KEYS:
-                extra_txt += ', {}={}'.format(k, v)
-        message = super().format(record)
-        return message + extra_txt
+from drf_logger.formatters import SimpleExtraFormatter
 
 
 def get_default_logger(name: str) -> logging.Logger:

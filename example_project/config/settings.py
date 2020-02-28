8a76d7278f5c4ee2b9519a3be8cd345c39130e7f
yutayamazaki <tppymd@gmail.com>
2020-02-29 00:12:02 +0900

    Rename example_project to config

--- example_project/config/settings.py
+++ example_project/config/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'example_project.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -98,7 +98,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'example_project.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
